0edadd65262a4c9cdbed21e3ea672c34cfcff411
--global <--global>
2023-07-18 18:35:39 +0500

    first commit

---
+++ src/WeatherApp/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import TopNav from "./TopNav";
+import Application from "./Application";
+
+export default function Main(){
+    return(
+        <>
+         <TopNav />
+         <Application/>
+        </>
+    )
+}
---
+++ src/WeatherApp/TopNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TopNav(){
+    return(
+    
+        <div className="topNav">
+            <h3> Sayyodbek Ibrohimov | Weather App</h3>
+        </div>
+       
+    )
+}
